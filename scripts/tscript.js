cbf1238b63830121f31b838344aa13afb4c4f997
Oz Elentok <oz.elen@gmail.com>
2012-08-05 11:26:54 +0300

    added countdown time edit through UI and Start/Stop button

--- scripts/tscript.js
+++ scripts/tscript.js
@@ -6,25 +6,23 @@ var timeController = {
 	targetTime: null,
 	
 	startCountdown : function() {
-		var regResults = /\?tc=(\d+)-(\d+)-(\d+)/.exec(window.location.href);
-		if (regResults == null) {
-			hours = 0;
-			mins = 0;
-			secs = 10;
-		}
-		else {
-			var hours = parseInt(regResults[1]);
-			var mins = parseInt(regResults[2]);
-			var secs = parseInt(regResults[3]);
-		}
-		if (mins >= 60)
-			mins = 0;
-		if (secs >= 60)
-			secs = 0;
-		document.getElementById("hourBox").innerHTML = this.formatNum(hours);
-		document.getElementById("minBox").innerHTML = this.formatNum(mins);
-		document.getElementById("secBox").innerHTML = this.formatNum(secs);
+		var hours = parseInt(document.getElementById("hourSelect").value);
+		var mins = parseInt(document.getElementById("minSelect").value);
+		var secs = parseInt(document.getElementById("secSelect").value);
+		document.getElementById("hourDisp").innerHTML = this.formatNum(hours);
+		document.getElementById("minDisp").innerHTML = this.formatNum(mins);
+		document.getElementById("secDisp").innerHTML = this.formatNum(secs);
 		this.targetTime = new Date(Date.now() + (((hours * 60 + mins) * 60 + secs) * 1000));
+		this.timerInt = window.setInterval(function() {
+			timeController.tick(); } , 450);
+	},
+	
+	stopCountdown : function() {
+		document.getElementById("hourSelect").value = document.getElementById("hourDisp").innerHTML;
+		document.getElementById("minSelect").value = document.getElementById("minDisp").innerHTML;
+		document.getElementById("secSelect").value = document.getElementById("secDisp").innerHTML;
+		this.targetTime = null;
+		window.clearInterval(this.timerInt);
 	},
 	
 	formatNum : function(num) {
@@ -36,9 +34,9 @@ var timeController = {
 	tick : function() {
 		var currentTime = new Date();
 		var diffTime = new Date(this.targetTime.getTime() - currentTime.getTime());
-		document.getElementById("hourBox").innerHTML = this.formatNum(diffTime.getUTCHours());
-		document.getElementById("minBox").innerHTML = this.formatNum(diffTime.getUTCMinutes());
-		document.getElementById("secBox").innerHTML = this.formatNum(diffTime.getUTCSeconds());
+		document.getElementById("hourDisp").innerHTML = this.formatNum(diffTime.getUTCHours());
+		document.getElementById("minDisp").innerHTML = this.formatNum(diffTime.getUTCMinutes());
+		document.getElementById("secDisp").innerHTML = this.formatNum(diffTime.getUTCSeconds());
 		
 		if (diffTime.getTime() < 1000) {
 			window.clearInterval(this.timerInt);
@@ -48,6 +46,30 @@ var timeController = {
 		
 	} // tick end
 };
-timeController.startCountdown();
-timeController.timerInt = window.setInterval("timeController.tick()", 450);
+
+var UIManager = {
+	
+	visible: 's', // 's' - select, 'd' - display
+	timeSelect: document.getElementById("selectTime"),
+	timeDisplay: document.getElementById("dispTime"),
+	buttonToggle: document.getElementById("toggler"),
+	
+	toggleCountdown : function() {
+		if (this.visible == 's') {
+			this.timeSelect.style.display = "none";
+			this.timeDisplay.style.display = "block";
+			this.buttonToggle.value = "Stop Countdown";
+			this.visible = 'd';
+			timeController.startCountdown();
+		}
+		else {
+			this.timeSelect.style.display = "block";
+			this.timeDisplay.style.display = "none";
+			this.buttonToggle.value = "Start Countdown";
+			this.visible = 's';
+			timeController.stopCountdown();
+		}
+	}
+	
+};
 
